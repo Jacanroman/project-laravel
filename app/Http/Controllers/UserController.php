952340f794780c78dad5ddc5fa41a0008b2fd11c
= <=>
2021-01-10 09:29:03 +0000

    update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,13 +17,15 @@ class UserController extends Controller
     //recibira una Request y un objeto de tipo request
     public function update(Request $request){
 
-        $id = \Auth::user()->id;
+        //CONSEGUIR USUARIO IDENTIFICADO
+        $user = \Auth::user();
+        $id = $user->id;
 
-        //validamos los datos
+        //VALIDACION DEL FORMULARIO
         // unique:users significa que sea unico en la tabla usuarios
         // nick'.$id la excepcion es cuando el nick coincide con
         //el nick del usuario con el id
-        
+
         $validate = $this->validate($request, [
             'name'=>'required|string|max:255',
             'surname'=>'required|string|max:255',
@@ -31,8 +33,9 @@ class UserController extends Controller
             'email'=>'required|string|email|max:255|unique:users,email,'.$id,
         ]);
 
-        //creamob variables y las rellenamos con los
-        //datos del request
+        
+        //RECOGER DATOS DEL FORMULARIO
+        //creamob variables y las rellenamos con los datos del request
         
         $name = $request->input('name');
         $surname = $request->input('surname');
@@ -45,5 +48,19 @@ class UserController extends Controller
         var_dump($email);
         die();
         */
+
+        //ASIGNAR NUEVOS VALORES AL OBJETO DE USUARIO
+        /*como son propiedades publicas le podemos 
+        asignar directamente el valor que tiene  */    
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nick = $nick;
+        $user->email = $email;
+
+        //EJECUTAR CONSULTA Y CAMBIO EN LA BASE DE DATOS
+        $user->update();
+
+        return redirect()->route('config')
+                        ->with(['message'=>'Usuario actualizado correctamente']);
     }
 }
