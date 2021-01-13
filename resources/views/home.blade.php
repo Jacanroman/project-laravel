26c3b2dc8c528de6e7d69a9eea225ed376dfdbcb
= <=>
2021-01-13 23:08:20 +0000

    styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,11 +28,20 @@
                         <div class="image-container">
                             <img src="{{ route('image.file',['filename' => $image->image_path])}}" />
                         </div>
-                        <div class="likes">
-                        </div>
+                        
                         <div class="description">
                             <span class="nickname">{{'@'.$image->user->nick}}</span>
                             <p> {{$image->description}} </p>
+                        </div>
+
+                        <div class="likes">
+                            <img src="{{asset('img/heart-68-64black.png')}}" />
+                        </div>
+
+                        <div class="comments">
+                            <a href="" class="btn btn-sm btn-warning btn-comments">
+                                Comentarios
+                            </a>
                         </div>
                     </div>
                 </div>
