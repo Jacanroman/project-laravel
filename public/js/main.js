789e41bbe7b5e3544f414f90135c403cc100974c
= <=>
2021-02-06 10:12:26 +0000

    cambiar el color del like usando jquery

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function(){
+    //comprobar si jquery esta cargado en le proyecto
+    //$('body').css("background",'red');
+
+    $('.btn-like').css('cursor','pointer');
+    $('.btn-dislike').css('cursor','pointer');
+
+    //Boton de like
+    function like(){
+        $('.btn-like').unbind('click').click(function(){
+            console.log('like');
+            $(this).addClass('btn-dislike').removeClass('btn-like');
+            $(this).attr('src','img/heart-68-64red.png');
+            dislike();
+        });
+    }
+    like();
+
+    //Boton de dislike
+    function dislike(){
+        $('.btn-dislike').unbind('click').click(function(){
+            console.log('dislike');
+            $(this).addClass('btn-like').removeClass('btn-dislike');
+            $(this).attr('src','img/heart-68-64black.png');
+            like();
+        });
+    }
+    dislike();
+
+});
