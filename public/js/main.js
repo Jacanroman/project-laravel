f5ceaee603e0dfdd4a9c8ebde65a64b86b988ce1
= <=>
2021-02-06 10:34:15 +0000

    peticion ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+let url = 'http://project-laravel.test';
+
 window.addEventListener("load", function(){
     //comprobar si jquery esta cargado en le proyecto
     //$('body').css("background",'red');
@@ -11,6 +13,19 @@ window.addEventListener("load", function(){
             console.log('like');
             $(this).addClass('btn-dislike').removeClass('btn-like');
             $(this).attr('src','img/heart-68-64red.png');
+
+            $.ajax({
+                url: url+'/like/'+$(this).data('id'),
+                type: 'GET',
+                success: function(response){
+                    if(response.like){
+                        console.log("has dado like a la publicacion");
+                    }else{
+                        console.log("error al dar like");
+                    } 
+                }
+            });
+
             dislike();
         });
     }
@@ -22,9 +37,21 @@ window.addEventListener("load", function(){
             console.log('dislike');
             $(this).addClass('btn-like').removeClass('btn-dislike');
             $(this).attr('src','img/heart-68-64black.png');
+
+            $.ajax({
+                url: url+'/dislike/'+$(this).data('id'),
+                type: 'GET',
+                success: function(response){
+                    if(response.like){
+                        console.log("has dado dislike a la publicacion");
+                    }else{
+                        console.log("error al dar dislike");
+                    } 
+                }
+            });
+
             like();
         });
     }
     dislike();
-
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,9 +48,9 @@
                             @endforeach
 
                             @if($user_like)
-                                <img src="{{asset('img/heart-68-64red.png')}}" class="btn-dislike" />
+                                <img src="{{asset('img/heart-68-64red.png')}}" data-id="{{$image->id}}" class="btn-dislike" />
                             @else
-                                <img src="{{asset('img/heart-68-64black.png')}}" class="btn-like" />
+                                <img src="{{asset('img/heart-68-64black.png')}}" data-id="{{$image->id}}"  class="btn-like" />
                             @endif
 
                             <span class="number_likes">{{count($image->likes)}}</span>
