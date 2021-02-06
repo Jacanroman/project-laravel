9e2ff2c150b62ff06146a362f3ae29c3c0e31560
= <=>
2021-02-06 19:11:20 +0000

    styles

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
-            <div class="data-user">
+            <div class="profile-user">
                 
                     @if($user->image)
                         <div class="container-avatar">
@@ -18,7 +18,11 @@
                     <h2>{{$user->name .' '. $user->surname}}</h2>
                     <p>{{'Se unio: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
                 </div>
+                <div class="clearfix"></div>
+                <hr/>
             </div>
+
+            <div class="clearfix"></div>
 
             @foreach($user->images as $image)
                 @include('includes.image',['image'=>$image])
