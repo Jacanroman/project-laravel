aa10eeb42e1cda471b55584f8f72843ea5f377dd
= <=>
2021-02-06 18:57:17 +0000

    mostrar datos en el perfil

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -5,6 +5,21 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
+            <div class="data-user">
+                
+                    @if($user->image)
+                        <div class="container-avatar">
+                            <img src="{{ route('user.avatar',['filename'=>$user->image])}}" />
+                        </div>
+                    @endif
+                
+                <div class="user-info">
+                    <h1>{{'@'.$user->nick}}</h1>
+                    <h2>{{$user->name .' '. $user->surname}}</h2>
+                    <p>{{'Se unio: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
+                </div>
+            </div>
+
             @foreach($user->images as $image)
                 @include('includes.image',['image'=>$image])
             @endforeach
